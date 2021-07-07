41fb956c76a5acefee8ca883f635730c82030679
Killian Dengreville <killian.dengreville@gmail.com>
2021-07-07 14:04:05 +0200

    add coordinates to groenland_infra

--- examples/groenland_infra/gen.py
+++ examples/groenland_infra/gen.py
@@ -9,167 +9,178 @@ import examples_generator.libgen as gen
 
 lengths = [
     1000, # 0
-    1000,
-    991,
-    2500,
-    5000,
-    2500, # 5
     2500,
     2500,
-    991,
-    5000,
-    5000, # 10
     5000,
     5000,
-    5000,
-    5000,
-    1000, # 15
-    1000,
-    1000,
+    5000, # 5
     1000,
     5000,
-    5000, # 20
+    5000,
+    5000,
+    1000, # 10
+    1000,
     5000,
     5000,
     5000,
+    5000, # 15
+    1000,
+    1000,
+    2000,
+    1000,
+    3000, # 20
+    1000,
+    500,
+    2500,
+    2500,
+    5500, # 25
     5000,
-    991, # 25
-    991,
-    992,
-    991,
+    5000,
+    1000,
     5000,
     5000, # 30
+    5500,
+    6500,
     5000,
     5000,
-    5000,
-    5000,
-    5000, # 35
-    5000,
-    991,
-    991,
-    991,
-    993, # 40
-    991,
-    992,
-    991,
-    1000,
-    991, # 45
-    992,
-    1000,
-    991,
-    993,
-    991, # 50
-    1000,
-    991,
-    1000,
+    5500, # 35
+    2000,
+    1500,
+    1500,
+    2000,
+    1500, # 40
+    1118,
+    6708,
+    1118,
+    707,
+    707, # 45
+    707,
+    707,
+    1118,
+    1118,
+    1118, # 50
+    1118,
+    11180,
+    11180,
     5000,
     5000, # 55
     5000,
-    5000,
-    10000,
-    1000,
-    1000, # 60
+    5385,
+    11000,
+    2000,
+    20000, # 60
+    2000,
     20000,
     1000,
-    1000,
-    20000,
-    1000, # 65
-    1000,
-    1000,
-    1000,
-    1000,
-    1000 # 70
+    1414,
+    1414, # 65
+    1414,
+    1414,
+    1118,
+    1118,
 ]
+
 infra = gen.Infra(lengths)
 
-# first line
-infra.add_switch(5, 0, 2)
-infra.add_link(5, 6)
-infra.add_switch(10, 8, 6)
-infra.add_link(10, 11)
-infra.add_link(11, 13)
-infra.add_switch(13, 67, 16)
-infra.add_switch(19, 15, 16)
-infra.add_link(19, 21)
-infra.add_link(21, 24)
-infra.add_switch(24, 55, 25)
-infra.add_switch(28, 26, 25)
-infra.add_switch(28, 29, 54)
-infra.add_link(29, 31)
-infra.add_link(31, 34)
-infra.add_link(34, 35)
-infra.add_link(35, 37)
-infra.add_switch(39, 37, 38)
-infra.add_switch(39, 41, 42)
-infra.add_switch(42, 44, 45)
-infra.add_switch(45, 48, 49)
-infra.add_switch(53, 49, 52)
-# second line
-infra.add_switch(1, 2, 3)
-infra.add_switch(7, 3, 4)
-infra.add_switch(7, 8, 9)
-infra.add_link(9, 12)
-infra.add_link(12, 14)
-infra.add_switch(14, 17, 68)
-infra.add_switch(20, 17, 18)
-infra.add_link(20, 22)
-infra.add_link(22, 23)
-infra.add_switch(23, 26, 27)
-infra.add_link(27, 30)
-infra.add_link(30, 32)
-infra.add_link(32, 33)
-infra.add_link(33, 36)
-infra.add_switch(36, 38, 40)
-infra.add_switch(43, 40, 41)
-infra.add_switch(43, 46, 47)
-infra.add_switch(50, 48, 46)
-infra.add_switch(50, 51, 52)
-# left line
-infra.add_link(55, 56)
-infra.add_link(54, 57)
-infra.add_switch(58, 56, 57)
-infra.add_switch(58, 59, 60)
-infra.add_switch(61, 59, 69)
-infra.add_switch(61, 62, 63)
-infra.add_switch(64, 62, 70)
-infra.add_switch(64, 65, 66)
+infra.add_switch(1, 0, 41, 0, -1000) # B
+infra.add_link(1, 2, 0, -3500) # C
+infra.add_switch(3, 2, 43, 0, -6000) # D
+infra.add_link(3, 4, 0, -11000) # E
+infra.add_link(4, 5, 0, -16000) # F
+infra.add_switch(5, 6, 44, 0, -21000) # G
+infra.add_switch(7, 6, 45, 0, -22000) # H
+infra.add_link(44, 45, -500, -21500) # W1
+infra.add_link(7, 8, 0, -27000) # I
+infra.add_link(8, 9, 0, -32000) # J
+infra.add_switch(9, 10, 54, 0, -37000) # K
+infra.add_switch(11, 10, 48, 0, -38000) # L
+infra.add_switch(11, 12, 55, 0, -39000) # M
+infra.add_link(12, 13, 0, -44000) # N
+infra.add_link(13, 14, 0, -49000) # O
+infra.add_link(14, 15, 0, -54000) # P
+infra.add_link(15, 16, 0, -59000) # Q
+infra.add_switch(17, 16, 49, 0, -60000) # R
+infra.add_switch(17, 18, 69, 0, -61000) # S
+infra.add_switch(18, 19, 52, 0, -63000) # T
+infra.add_switch(19, 20, 50, 0, -64000) # U
+infra.add_switch(21, 20, 51, 0, -67000) # V
+infra.add_switch(22, 23, 41, 1000, -500) # A1
+infra.add_switch(24, 23, 42, 1000, -3000) # B1
+infra.add_switch(24, 25, 43, 1000, -5500) # C1
+infra.add_link(25, 26, 1000, -11000) # E1
+infra.add_link(26, 27, 1000, -16000) # F1
+infra.add_switch(27, 28, 46, 1000, -21000) # G1
+infra.add_switch(29, 28, 47, 1000, -22000) # H1
+infra.add_link(46, 47, 1500, -21500) # Z1
+infra.add_link(29, 30, 1000, -27000) # I1
+infra.add_link(30, 31, 1000, -32000) # J1
+infra.add_switch(31, 32, 48, 1000, -37500) # K1
+infra.add_link(32, 33, 1000, -44000) # L1
+infra.add_link(33, 34, 1000, -49000) # M1
+infra.add_link(34, 35, 1000, -54000) # N1
+infra.add_switch(35, 36, 49, 1000, -59500) # O1
+infra.add_switch(37, 36, 69, 1000, -61500) # P1
+infra.add_switch(37, 38, 53, 1000, -63000) # Q1
+infra.add_switch(39, 38, 50, 1000, -64500) # R1
+infra.add_switch(39, 40, 51, 1000, -66500) # S1
+infra.add_link(54, 56, -5000, -37000) # B2
+infra.add_link(55, 57, -5000, -39000) # A2
+infra.add_switch(58, 56, 57, -10000, -37000) # C2
+infra.add_switch(58, 59, 64, -21000, -37000) # D2
+infra.add_link(64, 65, -22000, -38000) # F2
+infra.add_switch(60, 59, 65, -23000, -37000) # E2
+infra.add_switch(60, 61, 66, -43000, -37000) # G2
+infra.add_link(66, 67, -44000, -38000) # I2
+infra.add_switch(62, 61, 67, -45000, -37000) # H2
+infra.add_switch(62, 63, 68, -65000, -37000) # J2
+
+infra.set_buffer_stop_coordinates(63, -66000, -37000) # K2
+infra.set_buffer_stop_coordinates(68, -66000, -38000) # L2
+infra.set_buffer_stop_coordinates(0, 0, 0) # A
+infra.set_buffer_stop_coordinates(22, 1000, 0) # Z
+infra.set_buffer_stop_coordinates(42, 7000, 0) # D1
+infra.set_buffer_stop_coordinates(52, -10000, -68000) # V1
+infra.set_buffer_stop_coordinates(21, 0, -68000) # W
+infra.set_buffer_stop_coordinates(40, 1000, -68000) # T1
+infra.set_buffer_stop_coordinates(53, 11000, -68000) # U1
 
 # build the trains
 sim = gen.Simulation(infra)
-sim.add_schedule(0, 0, 66)
+
+sim.add_schedule(0, 22, 68)
 
 # build the successions
 succession = gen.Succession()
 
-succession.add_table(5, 0, 2, [])
-succession.add_table(10, 8, 6, [])
-succession.add_table(13, 67, 16, [])
-succession.add_table(19, 15, 16, [])
-succession.add_table(24, 55, 25, [])
-succession.add_table(28, 26, 25, [])
-succession.add_table(28, 29, 54, [])
-succession.add_table(39, 37, 38, [])
-succession.add_table(39, 41, 42, [])
-succession.add_table(42, 44, 45, [])
-succession.add_table(45, 48, 49, [])
-succession.add_table(53, 49, 52, [])
-succession.add_table(1, 2, 3, [])
-succession.add_table(7, 3, 4, [])
-succession.add_table(7, 8, 9, [])
-succession.add_table(14, 17, 68, [])
-succession.add_table(20, 17, 18, [])
-succession.add_table(23, 26, 27, [])
-succession.add_table(36, 38, 40, [])
-succession.add_table(43, 40, 41, [])
-succession.add_table(43, 46, 47, [])
-succession.add_table(50, 48, 46, [])
-succession.add_table(50, 51, 52, [])
-succession.add_table(58, 56, 57, [])
-succession.add_table(58, 59, 60, [])
-succession.add_table(61, 59, 69, [])
-succession.add_table(61, 62, 63, [])
-succession.add_table(64, 62, 70, [])
-succession.add_table(64, 65, 66, [])
+succession.add_table(1, 0, 41, []) # B
+succession.add_table(3, 2, 43, []) # D
+succession.add_table(5, 6, 44, []) # G
+succession.add_table(7, 6, 45, []) # H
+succession.add_table(9, 10, 54, []) # K
+succession.add_table(11, 10, 48, []) # L
+succession.add_table(11, 12, 55, []) # M
+succession.add_table(17, 16, 49, []) # R
+succession.add_table(17, 18, 69, []) # S
+succession.add_table(18, 19, 52, []) # T
+succession.add_table(19, 20, 50, []) # U
+succession.add_table(21, 20, 51, []) # V
+succession.add_table(22, 23, 41, []) # A1
+succession.add_table(24, 23, 42, []) # B1
+succession.add_table(24, 25, 43, []) # C1
+succession.add_table(27, 28, 46, []) # G1
+succession.add_table(29, 28, 47, []) # H1
+succession.add_table(31, 32, 48, []) # K1
+succession.add_table(35, 36, 49, []) # O1
+succession.add_table(37, 36, 69, []) # P1
+succession.add_table(37, 38, 53, []) # Q1
+succession.add_table(39, 38, 50, []) # R1
+succession.add_table(39, 40, 51, []) # S1
+succession.add_table(58, 56, 57, []) # C2
+succession.add_table(58, 59, 64, []) # D2
+succession.add_table(60, 59, 65, []) # E2
+succession.add_table(60, 61, 66, []) # G2
+succession.add_table(62, 61, 67, []) # H2
+succession.add_table(62, 63, 68, []) # J2
 
 gen.write_json("config.json", gen.CONFIG_JSON)
 gen.write_json("infra.json", infra.to_json())
--- examples_generator/libgen.py
+++ examples_generator/libgen.py
@@ -1,6 +1,5 @@
 from heapq import heappush, heappop
 import json
-import networkx
 
 def write_json(filename, data):
     out = open(filename, "w")
@@ -91,46 +90,6 @@ class Infra:
     def build_operational_points(self):
         self.json["operational_points"] = []
 
-    def build_endpoints_coords(self):
-        g = networkx.Graph()
-        nodeMap = dict()
-        node = 0
-        for utrack in range(self.nb_tracks):
-            if self.degree[utrack] == 1: 
-                nodeMap[uget_end(utrack)] = node
-                g.add_node(node)
-                node += 1
-        for ofirst, olast in self.links:
-            nodeMap[ofirst] = node
-            nodeMap[olast] = node
-            g.add_node(node)
-            node += 1
-        for obase, oleft, oright in self.switches:
-            nodeMap[obase] = node
-            nodeMap[oleft] = node
-            nodeMap[oright] = node
-            g.add_node(node)
-            node += 1
-        for utrack in range(self.nb_tracks):
-            g.add_edge(nodeMap[uget_begin(utrack)], nodeMap[uget_end(utrack)], length = 1 / self.lengths[utrack])
-
-        #layout = networkx.spring_layout(g, weight = 'length')
-        layout = networkx.planar_layout(g)
-        trueMean = sum(self.lengths) / len(self.lengths)
-        layoutMean = 0
-        for utrack in range(self.nb_tracks):
-            x1, y1 = layout[nodeMap[uget_begin(utrack)]]
-            x2, y2 = layout[nodeMap[uget_end(utrack)]]
-            layoutMean += ((x1-x2)**2 + (y1-y2)**2) ** 0.5
-        layoutMean /= len(self.lengths)
-        for k in layout:
-            layout[k] *= trueMean / layoutMean
-
-        for utrack in range(self.nb_tracks):
-            x1, y1 = layout[nodeMap[uget_begin(utrack)]]
-            x2, y2 = layout[nodeMap[uget_end(utrack)]]
-            self.json["track_sections"][utrack]["endpoints_coords"] = [[x1, y1], [x2, y2]]
-
     def build_track_sections(self):
         self.json["track_sections"] = []
         for utrack in range(self.nb_tracks):
@@ -140,11 +99,11 @@ class Infra:
                     "operational_points": [],
                     "route_waypoints": None,
                     "signals": [],
-                    "speed_sections": []
+                    "speed_sections": [],
+                    "endpoints_coords": [self.coordinates[uget_begin(utrack)], self.coordinates[uget_end(utrack)]]
                 })
         self.build_tde()
         self.build_signals()
-        self.build_endpoints_coords()
 
     def build_tde(self):
         for utrack in range(self.nb_tracks):
@@ -386,16 +345,25 @@ class Infra:
         else:
             return uget_end(utrack)
 
-    def add_link(self, ufirst, usecond):
+    def add_link(self, ufirst, usecond, x = 0, y = 0):
         ofirst = self.new_point(ufirst)
         osecond = self.new_point(usecond)
         self.links.append((ofirst, osecond))
+        self.coordinates[ofirst] = (x, y)
+        self.coordinates[osecond] = (x, y)
         
-    def add_switch(self, ubase, uleft, uright):
+    def add_switch(self, ubase, uleft, uright, x = 0, y = 0):
         obase = self.new_point(ubase)
         oleft = self.new_point(uleft)
         oright = self.new_point(uright)
         self.switches.append((obase, oleft, oright))
+        self.coordinates[obase] = (x, y)
+        self.coordinates[oleft] = (x, y)
+        self.coordinates[oright] = (x, y)
+
+    def set_buffer_stop_coordinates(self, utrack, x, y):
+        assert self.degree[utrack] == 1
+        self.coordinates[uget_end(utrack)] = (x, y)
 
     def to_json(self):
         for utrack in range(self.nb_tracks):
@@ -425,6 +393,7 @@ class Infra:
         self.degree = [0 for utrack in range(self.nb_tracks)]
         self.links = []
         self.switches = []
+        self.coordinates = [(0, 0) for otrack in range(2 * self.nb_tracks)]
 
 class Simulation:
 
